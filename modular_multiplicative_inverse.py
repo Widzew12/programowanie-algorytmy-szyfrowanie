9ce04e64adc8db7d443c6fe2d48d04ac3ab854d6
Widzew12 <122727731+Widzew12@users.noreply.github.com>
2024-12-17 08:08:05 +0100

    Update modular_multiplicative_inverse.py

--- modular_multiplicative_inverse.py
+++ modular_multiplicative_inverse.py
@@ -1,45 +1,47 @@
 # Python3 program to find multiplicative modulo
 # inverse using Extended Euclid algorithm.
 
+class MultiplicativeInverse:
+    def __init__(self):
+        self.x, self.y = 0, 1
+
 # Global Variables
 x, y = 0, 1
 
 # Function for extended Euclidean Algorithm
 
 
-def gcdExtended(a, b):
-    global x, y
-
+def gcdExtended(self, a, b):
     # Base Case
     if (a == 0):
-        x = 0
-        y = 1
+        self.x = 0
+        self.y = 1
         return b
 
     # To store results of recursive call
-    gcd = gcdExtended(b % a, a)
-    x1 = x
-    y1 = y
+    gcd = self.gcdExtended(b % a, a)
+    x1 = self.x
+    y1 = self.y
 
-    # Update x and y using results of recursive
-    # call
-    x = y1 - (b // a) * x1
-    y = x1
+    # Update x and y using results of recursive call
+    self.x = y1 - (b // a) * x1
+    self.y = x1
 
     return gcd
 
 
 def modInverse(A, M):
-
-    g = gcdExtended(A, M)
+    multiplicative_inverse = MultiplicativeInverse()
+    g = multplicative_inverse.gcdExtended(A, M)
     if (g != 1):
         print("Inverse doesn't exist")
-
+        return None
     else:
 
         # m is added to handle negative x
         res = (x % M + M) % M
         print("Modular multiplicative inverse is ", res)
+        return res
 
 
 # Driver Code
